45b11ce96d9bd3e48b3feef1e1099b5d451a7f25
Kyle Harris <k_harris95@live.com>
2018-01-24 05:43:29 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,7 +59,7 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-  holiday_hash.each { |holiday, supplies| 
+  holiday_hash.each { |holiday, supplies|
     supplies.each { |attribute, value| puts "#{attribute}: #{value}" }
   }
 
