de3e345a6bb31b2d76a7c4d44b2f3a3ecd1748a6
Kyle Harris <k_harris95@live.com>
2018-01-24 05:53:59 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,7 +65,7 @@ def all_supplies_in_holidays(holiday_hash)
       holiday_arr = holiday_arr.each { |word| word.capitalize}
       holiday = holiday_arr.join
     }
-    puts "#{holiday}:"
+    puts "#{holiday.to_s.capitalize}:"
     supplies.each { |attribute, value| puts "#{attribute.to_s}: #{value}" }
   }
 
