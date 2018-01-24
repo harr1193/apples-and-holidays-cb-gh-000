122baec53ec81f7af1f755d1d54cfa9dc6cfa6e8
Kyle Harris <k_harris95@live.com>
2018-01-24 05:59:27 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,8 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each { |holiday, supplies|
-    puts "#{holiday.to_s.capitalize}: #{supplies.flatten}"
+    puts "#{holiday.to_s.capitalize}:"
+    puts "#{supplies.flatten}"
   }
 
 end
